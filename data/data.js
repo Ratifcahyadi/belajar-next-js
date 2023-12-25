15ef418b35b3069ac5d45d243c6c98e9363b6fcd
Ratif Cahyadi <ratifcahyadi@students.amikom.ac.id>
2023-12-25 13:51:24 +0700

    learn next

---
+++ data/data.js
@@ -0,0 +1,123 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first: 'John',
+            last: 'Smith'
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '15 Minutes ago',
+    },
+    {
+        id: 1,
+        name: {
+            first: 'John',
+            last: 'Smith'
+        },
+        total: 2795.95,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '15 Minutes ago',
+    },
+    {
+        id: 2,
+        name: {
+            first: 'Chris',
+            last: 'Adam'
+        },
+        total: 1195.95,
+        status: 'Processing',
+        method: 'Paypal',
+        date: '23 Minutes ago',
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Sarah',
+            last: 'Smith'
+        },
+        total: 495.95,
+        status: 'Completed',
+        method: 'Visa',
+        date: '15 Hour ago',
+    },
+    {
+        id: 4,
+        name: {
+            first: 'Joseph',
+            last: 'Choo'
+        },
+        total: 150.45,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '2 Hour ago',
+    },
+    {
+        id: 5,
+        name: {
+            first: 'Steve',
+            last: 'Harding'
+        },
+        total: 175.25,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '2 Hour ago',
+    },
+    {
+        id: 6,
+        name: {
+            first: 'Laura',
+            last: 'Croft'
+        },
+        total: 150.45,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '2 Hour ago',
+    },
+    {
+        id: 6,
+        name: {
+            first: 'Laura',
+            last: 'Croft'
+        },
+        total: 150.45,
+        status: 'On Hold',
+        method: 'Paypal',
+        date: '2 Hour ago',
+    },
+    {
+        id: 8,
+        name: {
+            first: 'James',
+            last: 'Bond'
+        },
+        total: 689.45,
+        status: 'Completed',
+        method: 'Visa',
+        date: '7 Hour ago',
+    },
+    {
+        id: 9,
+        name: {
+            first: 'Haley',
+            last: 'Whiting'
+        },
+        total: 14.99,
+        status: 'Completed',
+        method: 'Paypal',
+        date: '1 Day ago',
+    },
+    {
+        id: 10,
+        name: {
+            first: 'Tin',
+            last: 'Thomas'
+        },
+        total: 218.95,
+        status: 'Completed',
+        method: 'Mastercard',
+        date: '1 Day ago',
+    },
+]
